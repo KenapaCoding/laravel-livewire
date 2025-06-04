90464c0c63558460fb8eb80ee7cd2c98ec40639b
ranggo pato <patoranggo@gmail.com>
2025-06-05 04:19:23 +0700

    actions

--- app/Livewire/CounterApp.php
+++ app/Livewire/CounterApp.php
@@ -8,6 +8,10 @@ class CounterApp extends Component
 {
     public $count = 100;
 
+    public function increase($num) {
+        $this->count = $this->count + $num;
+    }
+
     public function render()
     {
         return view('livewire.counter-app',[
--- resources/views/livewire/counter-app.blade.php
+++ resources/views/livewire/counter-app.blade.php
@@ -2,4 +2,7 @@
     {{-- The Master doesn't talk, he acts. --}}
     <p>Nama : {{$nama}}</p>
     <h1>Counter : {{$count}}</h1>
+    <button class="px-4 py-2 shadow-2xl bg-blue-400 text-white cursor-pointer rounded" wire:click="increase(4)">
+        Increment
+    </button>
 </div>
