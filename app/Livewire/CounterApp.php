3052861fcf1bedb837640c66a1f1f5a6244829ae
ranggo pato <patoranggo@gmail.com>
2025-06-05 14:49:50 +0700

    property, data binding, modifiers

--- app/Livewire/CounterApp.php
+++ app/Livewire/CounterApp.php
@@ -6,7 +6,9 @@ use Livewire\Component;
 
 class CounterApp extends Component
 {
-    public $count = 100;
+    public $count = 101;
+
+    public $panggilan = 'Boy';
 
     public function increase($num) {
         $this->count = $this->count + $num;
--- resources/views/livewire/counter-app.blade.php
+++ resources/views/livewire/counter-app.blade.php
@@ -2,6 +2,11 @@
     {{-- The Master doesn't talk, he acts. --}}
     <p>Nama : {{$nama}}</p>
     <h1>Counter : {{$count}}</h1>
+    <p>Panggilan : {{$panggilan}}</p>
+    <form wire:submit='$refresh' class="my-4">
+        <input wire:model='panggilan' type="text" class="border-1 border-black rounded px-2 py-1">
+        <button type="submit" class="bg-teal-500 text-white rounded px-2 py-1">Kirim</button>
+    </form>
     <button class="px-4 py-2 shadow-2xl bg-blue-400 text-white cursor-pointer rounded" wire:click="increase(4)">
         Increment
     </button>
