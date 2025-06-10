3ce844386835775dbaa4346544c5271ca59b24ae
ranggo pato <patoranggo@gmail.com>
2025-06-10 17:47:05 +0700

    nested component,props,mount

---
+++ resources/views/livewire/post-item.blade.php
@@ -0,0 +1,4 @@
+<div wire:key={{ $post['id'] }} class="shadow-lg px-4 py-3 m-4">
+    <h1 class="text-xl font-bold">{{$post['title']}}</h1>
+    <p>{{$post['content']}}</p>
+</div>
--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,9 +1,6 @@
 <div>
     {{-- Success is as dangerous as failure. --}}
     @foreach ($posts as $post)
-        <div wire:key={{ $post['id'] }} class="shadow-lg px-4 py-3 m-4">
-            <h1 class="text-xl font-bold">{{$post['title']}}</h1>
-            <p>{{$post['content']}}</p>
-        </div>
+        <livewire:post-item :post="$post" :key="$post['id']"/>
     @endforeach
 </div>
