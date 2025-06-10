ab9280c5f85f73b4beec0ae76d6ff7060cdca3f4
ranggo pato <patoranggo@gmail.com>
2025-06-10 23:01:48 +0700

    perbedaan cara passing data ke view

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -2,40 +2,26 @@
 
 namespace App\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class PostList extends Component
 {
-    public $posts = [
-        [
-            'id' => 1,
-            'title' => 'Belajar Livewire Dasar',
-            'content' => 'Livewire memudahkan pengembangan frontend menggunakan Laravel.'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Apa Itu Lifecycle Hook?',
-            'content' => 'Lifecycle hook di Livewire memungkinkan kita menangani proses seperti inisialisasi data.'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Penggunaan mount() di Livewire',
-            'content' => 'mount() dipanggil saat komponen pertama kali dibuat dan digunakan untuk setup data.'
-        ],
-        [
-            'id' => 4,
-            'title' => 'Perbandingan mount() dan boot()',
-            'content' => 'boot() digunakan untuk setup global, sementara mount() lebih spesifik untuk komponen.'
-        ],
-        [
-            'id' => 5,
-            'title' => 'Menampilkan Data Post di Komponen',
-            'content' => 'Kita bisa menggunakan looping di blade untuk menampilkan data post ke user.'
-        ],
-    ];
+    // public $posts;
+
+    public function delete(Post $post) {
+        $post->delete();
+        // $this->posts = Post::all();
+    }
+
+    // public function mount() {
+    //     $this->posts = Post::all();
+    // }
 
     public function render()
     {
-        return view('livewire.post-list');
+        return view('livewire.post-list', [
+            "posts" => Post::all()
+        ]);
     }
 }
--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,6 +1,9 @@
 <div>
     {{-- Success is as dangerous as failure. --}}
     @foreach ($posts as $post)
-        <livewire:post-item :post="$post" :key="$post['id']"/>
+        <livewire:post-item :post="$post" :key="$post->id"/>
+        <button wire:click="delete({{ $post->id }})" class="mx-6 px-4 py-2 rounded text-white bg-red-500">
+            Delete
+        </button>
     @endforeach
 </div>
